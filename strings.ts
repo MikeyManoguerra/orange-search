5543938b0bb21ad0b54c8bd134334cf3120be0bb
mikeymanoguerra <mikeymanoguerra@gmail.com>
2019-08-13 00:23:26 -0400

    ran in browser console, not sure where to go next, gapi, or gui

---
+++ strings.ts
@@ -0,0 +1,159 @@
+export const mondayString = `
+AZ - 08/17/2019 - 10:30 AM-4:30 PM - Walmart Supercenter, 5695 Highway 95 N, Lake Havasu City, AZ 86404
+CO - 08/17/2019 - 10:30 AM-4:30 PM - Walmart Supercenter, 1250 E Magnolia St, Fort Collins, CO 80524
+CO - 08/18/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 1250 E Magnolia St, Fort Collins, CO 80524
+ID - 08/15/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 5108 Cleveland Blvd, Caldwell, ID 83607
+ID - 08/18/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 5108 Cleveland Blvd, Caldwell, ID 83607
+IL - 08/16/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 4000 IL Route 173, Zion, IL 60099
+IL - 08/17/2019 - 10:30 AM-4:30 PM - Walmart Supercenter, 1100 S Randall Rd, Elgin, IL 60123
+IL - 08/17/2019 - 10:30 AM-4:30 PM - Walmart Supercenter, 1300 Des Plaines Ave, Forest Park, IL 60130
+IL - 08/17/2019 - 10:30 AM-4:30 PM - Walmart Supercenter, 820 S Rand Rd, Lake Zurich, IL 60047
+IL - 08/18/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 101 Dettro Dr, Mattoon, IL 61938
+IL - 08/18/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 2300 US Highway 34, Oswego, IL 60543
+IL - 08/18/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 3320 S Cicero Ave, Cicero, IL 60804
+IL - 08/19/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 475 E Il Route 173, Antioch, IL 60002
+IN - 08/15/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 790 Greensburg Commons Ctr, Greensburg, IN 47240
+IN - 08/16/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 505 Touring Dr, Auburn, IN 46706
+IN - 08/17/2019 - 10:30 AM-4:30 PM - Walmart Supercenter, 505 Touring Dr, Auburn, IN 46706
+IN - 08/17/2019 - 10:30 AM-4:30 PM - Walmart Supercenter, 790 Greensburg Commons Ctr, Greensburg, IN 47240
+IN - 08/18/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 505 Touring Dr, Auburn, IN 46706
+IN - 08/18/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 790 Greensburg Commons Ctr, Greensburg, IN 47240
+MA - 08/16/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 100 Valley Pkwy, Whitinsville, MA 01588
+MA - 08/16/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 20 Soojian Dr, Leicester, MA 01524
+MA - 08/17/2019 - 10:30 AM-4:30 PM - Walmart Supercenter, 20 Soojian Dr, Leicester, MA 01524
+MD - 08/16/2019 - 11:00 AM-4:00 PM - Walmart Supercenter, 6400A Ridge Rd Ste 1, Sykesville, MD 21784
+MD - 08/16/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 2320 N Hanover Pike, Hampstead, MD 21074
+MD - 08/16/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 3549 Russett Grn, Laurel, MD 20724
+MD - 08/17/2019 - 6:30 AM-10:30 AM - Walmart Supercenter, 3549 Russett Grn, Laurel, MD 20724
+MD - 08/18/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 3549 Russett Grn, Laurel, MD 20724
+MI - 08/15/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 1150 S Main St, Cheboygan, MI 49721
+MI - 08/16/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 1121 E Caro Rd, Caro, MI 48723
+MI - 08/16/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 1150 S Main St, Cheboygan, MI 49721
+MI - 08/17/2019 - 10:30 AM-4:30 PM - Walmart Supercenter, 1150 S Main St, Cheboygan, MI 49721
+MI - 08/18/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 1121 E Caro Rd, Caro, MI 48723
+MO - 08/15/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 18401 State Highway 13, Reeds Spring, MO 65737
+NC - 08/16/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 2330 US 19, Murphy, NC 28906
+NC - 08/17/2019 - 10:30 AM-4:30 PM - Walmart Supercenter, 1226 E Dixie Dr, Asheboro, NC 27203
+NC - 08/17/2019 - 11:00 AM-4:00 PM - Walmart Supercenter, 2330 US 19, Murphy, NC 28906
+NH - 08/17/2019 - 10:30 AM-4:30 PM - Walmart Supercenter, 85 State Route 101a, Amherst, NH 03031
+NH - 08/17/2019 - 11:00 AM-4:00 PM - Walmart Supercenter, 725 Gold St, Manchester, NH 03103
+NJ - 08/16/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 2106 Mount Holly Rd, Burlington, NJ 08016
+NJ - 08/17/2019 - 10:30 AM-4:30 PM - Walmart Supercenter, 1070 W Landis Ave, Vineland, NJ 08360
+NJ - 08/17/2019 - 10:30 AM-4:30 PM - Walmart Supercenter, 265 N Route 73, West Berlin, NJ 08091
+NJ - 08/18/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 2106 Mount Holly Rd, Burlington, NJ 08016
+NJ - 08/18/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 326 W Main St, Freehold, NJ 07728
+NY - 08/15/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 200 Dutch Meadows Ln, Schenectady, NY 12302
+NY - 08/16/2019 - 11:00 AM-4:00 PM - Walmart Supercenter, 5360 Southwestern Blvd, Hamburg, NY 14075
+NY - 08/16/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 200 Dutch Meadows Ln, Schenectady, NY 12302
+NY - 08/16/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 5360 Southwestern Blvd, Hamburg, NY 14075
+NY - 08/17/2019 - 10:30 AM-4:30 PM - Walmart Supercenter, 200 Dutch Meadows Ln, Schenectady, NY 12302
+NY - 08/18/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 200 Dutch Meadows Ln, Schenectady, NY 12302
+NY - 8/16/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 4238 Recreation Dr, Canandaigua, NY 14424
+OH - 08/16/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 1161 Trenton Ave, Findlay, OH 45840
+OH - 08/16/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 1355 S McCord Rd, Holland, OH 43528
+OH - 08/16/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 231 Bluebell Dr SW, New Philadelphia, OH 44663
+OH - 08/16/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 2500 Tiffin Ave, Findlay, OH 45840
+OH - 08/16/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 2801 W State Route 18, Tiffin, OH 44883
+OH - 08/17/2019 - 10:30 AM-4:30 PM - Walmart Supercenter, 1161 Trenton Ave, Findlay, OH 45840
+OH - 08/17/2019 - 10:30 AM-4:30 PM - Walmart Supercenter, 1355 S McCord Rd, Holland, OH 43528
+OH - 08/17/2019 - 10:30 AM-4:30 PM - Walmart Supercenter, 2500 Tiffin Ave, Findlay, OH 45840
+OH - 08/17/2019 - 10:30 AM-4:30 PM - Walmart Supercenter, 2826 East Harbor Rd, Port Clinton, OH 43452
+OH - 08/18/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 1161 Trenton Ave, Findlay, OH 45840
+OH - 08/18/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 1355 S McCord Rd, Holland, OH 43528
+OH - 08/18/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 2500 Tiffin Ave, Findlay, OH 45840
+OH - 08/18/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 340 Westwind Dr, Norwalk, OH 44857
+OH - 08/18/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 7235 Market Place Dr, Aurora, OH 44202
+OH - 08/19/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 1161 Trenton Ave, Findlay, OH 45840
+PA - 08/16/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 2010 Village Center Dr, Tarentum, PA 15084
+PA - 08/16/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 60 Noble Blvd, Carlisle, PA 17013
+PA - 08/17/2019 - 10:30 AM-4:30 PM - Walmart Supercenter, 2010 Village Center Dr, Tarentum, PA 15084
+PA - 08/17/2019 - 10:30 AM-4:30 PM - Walmart Supercenter, 495 Eisenhower Dr, Hanover, PA 17331
+PA - 08/17/2019 - 10:30 AM-4:30 PM - Walmart Supercenter, 60 Noble Blvd, Carlisle, PA 17013
+PA - 08/17/2019 - 10:30 AM-4:30 PM - Walmart Supercenter, 800 Commons Dr, Oxford, PA 19363
+PA - 08/17/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 6520 Carlisle Pike Ste 550, Mechanicsburg, PA 17050
+PA - 08/18/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 2010 Village Center Dr, Tarentum, PA 15084
+PA - 08/18/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 60 Noble Blvd, Carlisle, PA 17013
+VA - 08/15/2019 - 12:30 PM-6:30 PM - Walmart Neighborhood Mkt, 1720 E Little Creek Rd, Norfolk, VA 23518
+VA - 08/15/2019 - 12:30 PM-6:30 PM - Walmart Neighborhood Mkt, 1832 Kempsville Rd, Virginia Beach, VA 23464
+VA - 08/15/2019 - 12:30 PM-6:30 PM - Walmart Neighborhood Mkt, 3201 Holland Rd, Virginia Beach, VA 23453
+VA - 08/15/2019 - 12:30 PM-6:30 PM - Walmart Neighborhood Mkt, 673 Cedar Rd, Chesapeake, VA 23322
+VA - 08/17/2019 - 10:30 AM-4:30 PM - Walmart Neighborhood Mkt, 1832 Kempsville Rd, Virginia Beach, VA 23464
+VA - 08/17/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 700 James Madison Hwy, Warrenton, VA 20186
+VA - 08/18/2019 - 12:30 PM-5:00 PM - Walmart Neighborhood Mkt, 3201 Holland Rd, Virginia Beach, VA 23453
+VA - 08/18/2019 - 12:30 PM-6:30 PM - Walmart Neighborhood Mkt, 1720 E Little Creek Rd, Norfolk, VA 23518
+VA - 08/18/2019 - 12:30 PM-6:30 PM - Walmart Neighborhood Mkt, 1832 Kempsville Rd, Virginia Beach, VA 23464
+VA - 08/18/2019 - 12:30 PM-6:30 PM - Walmart Neighborhood Mkt, 673 Cedar Rd, Chesapeake, VA 23322
+VA - 08/18/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 201 Hillcrest Pkwy, Chesapeake, VA 23322
+VA - 08/18/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 700 James Madison Hwy, Warrenton, VA 20186
+VA - 08/18/2019 - 12:30 PM-6:30 PM - Walmart Supercenter, 900 Walmart Way, Midlothian, VA 23113
+VA - 08/19/2019 - 12:30 PM-6:30 PM - Walmart Neighborhood Mkt, 1720 E Little Creek Rd, Norfolk, VA 23518
+VA - 08/19/2019 - 12:30 PM-6:30 PM - Walmart Neighborhood Mkt, 1832 Kempsville Rd, Virginia Beach, VA 23464
+VA - 08/20/2019 - 12:30 PM-6:30 PM - Walmart Neighborhood Mkt, 3201 Holland Rd, Virginia Beach, VA 23453`
+
+export const testString = `AK - 8/31/2019 - 2:00 PM-6:00 PM - Walmart, 10096 KENAI SPUR HWY, KENAI, AK 99611
+AK - 8/31/2019 - 2:00 PM-6:00 PM - Walmart, 537 JOHANSEN EXPY, FAIRBANKS, AK 99701
+AZ - 8/24/2019 - 2:00 PM-6:00 PM - Walmart, 2750 S WOODLANDS VILLAGE BLVD, FLAGSTAFF, AZ 86001
+AZ - 8/31/2019 - 2:00 PM-6:00 PM - Walmart, 2601 E HUNTINGTON DR, FLAGSTAFF, AZ 86004
+AZ - 8/31/2019 - 2:00 PM-6:00 PM - Walmart, 6145 N 35TH AVE, PHOENIX, AZ 85017
+MT - 8/24/2019 - 2:00 PM-6:00 PM - Walmart, 1500 N 7TH AVE, BOZEMAN, MT 59715
+NC - 8/17/2019 - 2:00 PM-6:00 PM - Walmart, 200 WATAUGA VILLAGE DR, BOONE, NC 28607
+NC - 8/17/2019 - 2:00 PM-6:00 PM - Walmart, 210 WALMART PLZ, SYLVA, NC 28779
+ND - 8/24/2019 - 2:00 PM-6:00 PM - Walmart, 2551 32ND AVE S, GRAND FORKS, ND 58201
+ND - 8/24/2019 - 2:00 PM-6:00 PM - Walmart, 5755 GATEWAY DRIVE, GRAND FORKS, ND 58203
+ND - 8/31/2019 - 2:00 PM-6:00 PM - Walmart, 1625 COMMERCE DR, WAHPETON, ND 58075
+ND - 8/31/2019 - 2:00 PM-6:00 PM - Walmart, 4731 13TH AVE S, FARGO, ND 58103
+NE - 8/24/2019 - 2:00 PM-6:00 PM - Walmart, 5411 2ND AVE, KEARNEY, NE 68847
+NH - 8/24/2019 - 2:00 PM-6:00 PM - Walmart, 285 PLAINFIELD RD, WEST LEBANON, NH 03784
+NH - 8/24/2019 - 2:00 PM-6:00 PM - Walmart, 350 WINCHESTER ST, KEENE, NH 03431
+NH - 8/24/2019 - 2:00 PM-6:00 PM - Walmart, 683 TENNEY MOUNTAIN HWY, PLYMOUTH, NH 03264
+NH - 8/31/2019 - 2:00 PM-6:00 PM - Walmart, 2200 WOODBURY AVE, NEWINGTON, NH 03801
+NY - 8/17/2019 - 2:00 PM-6:00 PM - Walmart, 2 GANNETT DR, JOHNSON CITY, NY 13790
+NY - 8/24/2019 - 2:00 PM-6:00 PM - Walmart, 1000 STATE ROUTE 36, HORNELL, NY 14843
+NY - 8/24/2019 - 2:00 PM-6:00 PM - Walmart, 135 FAIRGROUNDS MEMORIAL PKWY, ITHACA, NY 14850
+NY - 8/24/2019 - 2:00 PM-6:00 PM - Walmart, 141 Washington Ave Extension , ALBANY, NY 12205
+NY - 8/24/2019 - 2:00 PM-6:00 PM - Walmart, 1869 PLAZA DR, OLEAN, NY 14760
+NY - 8/24/2019 - 2:00 PM-6:00 PM - Walmart, 25 CONSUMER SQ, PLATTSBURGH, NY 12901
+NY - 8/24/2019 - 2:00 PM-6:00 PM - Walmart, 341 STATE ROUTE 104, OSWEGO, NY 13126
+NY - 8/24/2019 - 2:00 PM-6:00 PM - Walmart, 4235 VETERAN DR, GENESEO, NY 14454
+NY - 8/24/2019 - 2:00 PM-6:00 PM - Walmart, 5054 STATE HIGHWAY 23, ONEONTA, NY 13820
+NY - 8/24/2019 - 2:00 PM-6:00 PM - Walmart, 6438 BASILE ROWE, EAST SYRACUSE, NY 13057
+NY - 8/24/2019 - 2:00 PM-6:00 PM - Walmart, 990 STATE ROUTE 5 AND 20, GENEVA, NY 14456
+NY - 8/31/2019 - 2:00 PM-6:00 PM - Walmart, 139 MERCHANT PL, COBLESKILL, NY 12043
+NY - 8/31/2019 - 2:00 PM-6:00 PM - Walmart, 7494 US HIGHWAY 11, POTSDAM, NY 13676
+PA - 8/24/2019 - 2:00 PM-6:00 PM - Walmart, 108 WASHINGTON TOWNE BLVD N, EDINBORO, PA 16412
+PA - 8/24/2019 - 2:00 PM-6:00 PM - Walmart, 1169 S MAIN ST, MANSFIELD, PA 16933
+PA - 8/24/2019 - 2:00 PM-6:00 PM - Walmart, 1270 YORK RD, GETTYSBURG, PA 17325
+PA - 8/24/2019 - 2:00 PM-6:00 PM - Walmart, 1566 W MAIN STREET EXT, GROVE CITY, PA 16127
+PA - 8/24/2019 - 2:00 PM-6:00 PM - Walmart, 167 HOGAN BLVD, MILL HALL, PA 17751
+PA - 8/24/2019 - 2:00 PM-6:00 PM - Walmart, 1825 DOWNS DR, ERIE, PA 16509
+PA - 8/24/2019 - 2:00 PM-6:00 PM - Walmart, 63 PERKINS RD, CLARION, PA 16214
+VA - 8/17/2019 - 2:00 PM-6:00 PM - Walmart, 1233 N LEE HWY, LEXINGTON, VA 24450
+VA - 8/24/2019 - 2:00 PM-6:00 PM - Walmart, 171 BURGESS ROAD, HARRISONBURG, VA 22801
+VA - 8/24/2019 - 2:00 PM-6:00 PM - Walmart, 1800 PERRY DR, FARMVILLE, VA 23901
+VA - 8/24/2019 - 2:00 PM-6:00 PM - Walmart, 6920 FOREST AVENUE, RICHMOND, VA 23230
+VA - 8/24/2019 - 2:00 PM-6:00 PM - Walmart, 7373 PEPPERS FERRY BLVD, FAIRLAWN, VA 24141
+VA - 8/24/2019 - 2:00 PM-6:00 PM - Walmart, 975 HILTON HEIGHTS RD, CHARLOTTESVILLE, VA 22901`
+
+
+export const fullEmailTest = `
+AL - 8/3/2019 - 9:00 AM-1:00 PM - Family Dollar, ALICEVILLE, , AL 35442
+DE - 8/3/2019 - 8:00 AM-2:00 PM - Family Dollar, SEAFORD, , DE 19973
+DE - 8/3/2019 - 9:00 AM-1:00 PM - Family Dollar, SEAFORD, , DE 19973
+IL - 8/3/2019 - 8:00 AM-2:00 PM - Family Dollar, WAUKEGAN, , IL 60085
+IL - 8/3/2019 - 9:00 AM-1:00 PM - Family Dollar, CHICAGO, , IL 60629
+IL - 8/3/2019 - 9:00 AM-1:00 PM - Family Dollar, CHICAGO, , IL 60647
+IL - 8/3/2019 - 9:00 AM-1:00 PM - Family Dollar, LYONS, , IL 60534
+KY - 8/3/2019 - 9:00 AM-1:00 PM - Family Dollar, STURGIS, , KY 42459
+MD - 8/3/2019 - 8:00 AM-2:00 PM - Family Dollar, HAGERSTOWN, , MD 21742
+MD - 8/3/2019 - 9:00 AM-1:00 PM - Family Dollar, BALTIMORE, , MD 21239
+MD - 8/3/2019 - 9:00 AM-1:00 PM - Family Dollar, HAGERSTOWN, , MD 21742
+MI - 8/3/2019 - 8:00 AM-2:00 PM - Family Dollar, MIO, , MI 48647
+MI - 8/3/2019 - 9:00 AM-1:00 PM - Family Dollar, GRAND RAPIDS, , MI 49503
+MI - 8/3/2019 - 9:00 AM-1:00 PM - Family Dollar, MIO, , MI 48647
+PA - 8/3/2019 - 8:00 AM-2:00 PM - Family Dollar, SHARON HILL, , PA 19079
+PA - 8/3/2019 - 9:00 AM-1:00 PM - Family Dollar, SHARON HILL, , PA 19079
+SC - 8/3/2019 - 9:00 AM-1:00 PM - Family Dollar, ANDERSON, , SC 29624
+SC - 8/3/2019 - 9:00 AM-1:00 PM - Family Dollar, ELGIN, , SC 29045
+SC - 8/3/2019 - 9:00 AM-1:00 PM - Family Dollar, HARDEEVILLE, , SC 29927
+SC - 8/3/2019 - 9:00 AM-1:00 PM - Family Dollar, LONGS, , SC 29568
+SC - 8/3/2019 - 9:00 AM-1:00 PM - Family Dollar, UNION, , SC 29379`
